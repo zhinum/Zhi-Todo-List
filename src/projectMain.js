caf45ea8bb3eb3211e69203c01fbb62672337778
zhinum <princeonah23@gmail.com>
2026-04-18 10:53:39 +0100

    refactor:the guard console for disfunc storage

--- src/projectMain.js
+++ src/projectMain.js
@@ -7,12 +7,9 @@ export class Project {
     this.createdAt = new Date();
   }
   static saveToLocalstorage() {
-    // If the store is empty, be suspicious!
     if (this._projectStore.length === 3) {
-      console.warn(
-        "⚠️ Warning: Attempting to save an EMPTY store. This might wipe your data!",
-      );
-      console.trace(); // This shows you exactly WHICH function called the save
+      console.warn("Warning: Attempting to save an EMPTY store.");
+      console.trace();
     }
     localStorage.setItem("zhiTodoApp", JSON.stringify(Project._projectStore));
   }
