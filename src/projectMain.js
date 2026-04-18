435f5c95be4806cc48a7adac63a7919b98e02d64
zhinum <princeonah23@gmail.com>
2026-04-18 10:51:27 +0100

    feat:initialized local storage

--- src/projectMain.js
+++ src/projectMain.js
@@ -6,6 +6,31 @@ export class Project {
     this.todos = [];
     this.createdAt = new Date();
   }
+  static saveToLocalstorage() {
+    // If the store is empty, be suspicious!
+    if (this._projectStore.length === 3) {
+      console.warn(
+        "⚠️ Warning: Attempting to save an EMPTY store. This might wipe your data!",
+      );
+      console.trace(); // This shows you exactly WHICH function called the save
+    }
+    localStorage.setItem("zhiTodoApp", JSON.stringify(Project._projectStore));
+  }
+  static loadFromLocalStorage() {
+    const savedJSON = localStorage.getItem("zhiTodoApp");
+    if (savedJSON) {
+      const rawData = JSON.parse(savedJSON);
+      this._projectStore = rawData.map((project) => {
+        project.createdAt = new Date(project.createdAt);
+
+        project.todos = project.todos.map((todo) => {
+          todo.createdAt = new Date(todo.createdAt);
+          return todo;
+        });
+        return project;
+      });
+    }
+  }
   static get ProjectStore() {
     return this._projectStore;
   }
